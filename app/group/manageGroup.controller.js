f1ded49ecef58daacda7f87e04740d5c8b2979cf
Deepu Vijayan <deepu.vijayan2007@gmail.com>
2017-09-21 08:25:25 +0530

    datatable config

--- app/group/manageGroup.controller.js
+++ app/group/manageGroup.controller.js
@@ -1,3 +1,38 @@
 erestaurant.controller("ManageGroupControl",["$scope", "apiService", "$state", function($scope, apiService, $state){
     
+    $scope.selected = [];
+    $scope.query = {
+        order: 'name',
+        limit: 5,
+        page: 1
+    };
+    $scope.options = {
+        rowSelection: false,
+        multiSelect: false,
+        autoSelect: true,
+        decapitate: false,
+        largeEditDialog: false,
+        boundaryLinks: false,
+        limitSelect: true,
+        pageSelect: true
+    };
+    $scope.limitOptions = [5, 10, 15];
+
+    function init(){
+        
+        apiService.manageGroup().then(function(data){
+            $scope.groups = data;
+        },
+        function(){
+            
+        })
+    }
+    $scope.logOrder = function (order) {
+        console.log('order: ', order);
+    };
+    $scope.logPagination = function (page, limit) {
+        console.log('page: ', page);
+        console.log('limit: ', limit);
+    }
+    init();
 }])
--- js/services/api.service.js
+++ js/services/api.service.js
@@ -1,13 +1,13 @@
 erestaurant.factory("apiService",["$http", "$q", "baseUrlService", function ($http, $q, baseUrlService){
     var service = {};
     service.login = login;
+    service.manageGroup = manageGroup;
     return service;
 
     function login(data){
         var def = $q.defer();
         var baseUrl = baseUrlService.getBaseUrl(true);
         var url = baseUrl + "/login";
-        console.log(data);
 
         // $http.post(url, data).then(function (data) {
         //     def.resolve(data.data);
@@ -17,5 +17,39 @@ erestaurant.factory("apiService",["$http", "$q", "baseUrlService", function ($ht
         def.resolve();
         return def.promise;
 
+    }
+
+    function manageGroup(){
+        var def = $q.defer();
+        var baseUrl = baseUrlService.getBaseUrl(true);
+        var url = baseUrl + "/manageGroup";
+
+        // $http.post(url, data).then(function (data) {
+        //     def.resolve(data.data);
+        // }, function (error) {
+        //     def.reject("Network/Connection Error");
+        // });
+        var data ={
+            "count": 15,
+            "records":[
+            {"name":"Alfreds Futterkiste","members":"Berlin","status":1},
+            {"name":"Ana Trujillo Emparedados y helados","members":"México D.F.","status":1},
+            {"name":"Antonio Moreno Taquería","members":"México D.F.","status":0},
+            {"name":"Around the Horn","members":"London","status":0},
+            {"name":"B's Beverages","members":"London","status":1},
+            {"name":"Berglunds snabbköp","members":"Luleå","status":0},
+            {"name":"Blauer See Delikatessen","members":"Mannheim","status":0},
+            {"name":"Blondel père et fils","members":"Strasbourg","status":1},
+            {"name":"Bólido Comidas preparadas","members":"Madrid","status":1},
+            {"name":"Bon app'","members":"Marseille","status":1},
+            {"name":"Bottom-Dollar Marketse","members":"Tsawassen","status":0},
+            {"name":"Cactus Comidas para llevar","members":"Buenos Aires","status":0},
+            {"name":"Centro comercial Moctezuma","members":"México D.F.","status":1},
+            {"name":"Chop-suey Chinese","members":"Bern","status":1},
+            {"name":"Comércio Mineiro","members":"São Paulo","status":1}
+            ]
+        } 
+        def.resolve(data);
+        return def.promise;
     }
 }]);
